4fcdb73bac6a8a217847babee00b8c376236cfa2
Vinayak Jadhav <vjadhav1010@gmail.com>
2026-03-02 09:30:27 +0530

    UI - header and input components

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,13 +1,35 @@
-import AuthInputs from "./components/AuthInputs.jsx";
-import Header from "./components/Header.jsx";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <Header />
-      <main>
-        <AuthInputs />
-      </main>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className="text-3xl font-bold underline">Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
+
+export default App;
